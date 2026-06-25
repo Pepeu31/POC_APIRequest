a7144226b1cdb0a78a97d6c6d17031b7b258f40c
Pepeu31 <127160668+Pepeu31@users.noreply.github.com>
2026-06-25 16:44:11 -0300

    feat: Implemented API Logic

---
+++ POC_APIRequest/POC_APIRequest/Views/HeroView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroView.swift
+//  POC_APIRequest
+//
+//  Created by Pedro Henrique L. Moreiras on 25/06/26.
+//
+
+import SwiftUI
+
+struct HeroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroView()
+}
