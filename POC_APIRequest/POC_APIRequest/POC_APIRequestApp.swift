59f038fb5b3390cb32b117804b30795d1a731131
Pepeu31 <127160668+Pepeu31@users.noreply.github.com>
2026-06-25 14:20:50 -0300

    Add: Initial Project Commit

---
+++ POC_APIRequest/POC_APIRequest/POC_APIRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_APIRequestApp.swift
+//  POC_APIRequest
+//
+//  Created by Pedro Henrique L. Moreiras on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct POC_APIRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
